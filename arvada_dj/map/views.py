afee567d00306c84e67b58502769835717d0b3b3
Mike Bannister <mikebannis@gmail.com>
2019-09-18 11:08:55 -0600

    Add navbar to top of all pages and fix map height.

--- arvada_dj/map/views.py
+++ arvada_dj/map/views.py
@@ -6,6 +6,6 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def index(request):
-    template = loader.get_template('map/index2.html')
+    template = loader.get_template('map/map.html')
     context = {}
     return HttpResponse(template.render(context, request))
