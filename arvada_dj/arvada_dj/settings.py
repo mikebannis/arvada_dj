fda52920ee781a5f180076f974ddbab94b38f6cf
Mike Bannister <mikebannis@gmail.com>
2019-10-01 08:53:55 -0600

    Added localhost to ALLOWED_HOSTS

--- arvada_dj/arvada_dj/settings.py
+++ arvada_dj/arvada_dj/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['respec.dev', 'www.respec.dev']
+ALLOWED_HOSTS = ['respec.dev', 'www.respec.dev', '127.0.0.1', 'localhost']
 
 APPEND_SLASH=False 
 
