4683aced05d25390c8c4d3a4f2239306b9908309
Mike Bannister <mikebannis@gmail.com>
2019-10-21 23:22:39 -0600

    Use OSM for admin

--- arvada_dj/comment/admin.py
+++ arvada_dj/comment/admin.py
@@ -1,16 +1,16 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 from comment.models import Comment, Response, Question, Assumption
 
 
-class AssumptionAdmin(admin.ModelAdmin):
+class AssumptionAdmin(admin.OSMGeoAdmin):
     list_filter = ('status', 'generation', 'num_responses')
 
 
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(admin.OSMGeoAdmin):
     list_filter = ('status', 'generation', 'author', 'num_responses')
 
 
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(admin.OSMGeoAdmin):
     list_filter = ('status', 'generation', 'num_responses')
 
 
